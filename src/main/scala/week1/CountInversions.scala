5728f5ce01ab31782eba5441a3acaebea2103927
davidalk <david.alkanani@gmail.com>
2014-05-13 15:20:40 +0100

    build sortAndCount method

--- src/main/scala/week1/CountInversions.scala
+++ src/main/scala/week1/CountInversions.scala
@@ -13,7 +13,7 @@ object CountInversions {
     stringArray.map(_.toInt)
   }
 
-  def merge(a: Array[Int], b: Array[Int]): Array[Int] = {
+  def mergeAndCout(a: Array[Int], b: Array[Int]): Array[Int] = {
     val outputSize = a.length + b.length
     val returnArray = Array.fill(outputSize)(0)
     var aIndex, bIndex = 0
@@ -29,5 +29,18 @@ object CountInversions {
       }
     }
     returnArray
+  }
+
+  def sortAndCount(in: Array[Int]): Array[Int] = {
+    var outArray = Array[Int]()
+    if (in.size == 1)
+      outArray = in
+    else {
+      val splitIndex = (in.length / 2).floor.toInt
+      val a = sortAndCount(in.slice(0, splitIndex))
+      val b = sortAndCount(in.slice(splitIndex, in.length))
+      outArray = mergeAndCout(a, b)
+    }
+    outArray
   }
 }
--- src/test/scala/week1/CountInversionsTest.scala
+++ src/test/scala/week1/CountInversionsTest.scala
@@ -10,12 +10,18 @@ class CountInversionsTest extends FlatSpec {
     assert(expected.deep == result.deep)
   }
   
-  "The merge function" should "combine two sorted arrays and return an ordered array" in {
+  "The mergeAndCout function" should "combine two sorted arrays and return an ordered array" in {
     val a = Array(3, 8, 10, 50, 100)
     val b = Array(2, 15, 50, 88, 92, 200, 350)
     val expected = Array(2, 3, 8, 10, 15, 50, 50, 88, 92, 100, 200, 350)
-    val result = CountInversions.merge(a, b)
+    val result = CountInversions.mergeAndCout(a, b)
     assert(expected.deep == result.deep)
   }
 
+  "The sourtAndCount function" should "return a fully sorted array" in {
+    val input = Array(100, 32, 9, 17, 88, 380, 1253, 48, 39, 5)
+    val expected = Array(5, 9, 17, 32, 39, 48, 88, 100, 380, 1253)
+    val result = CountInversions.sortAndCount(input)
+    assert(expected.deep == result.deep)
+  }
 }
