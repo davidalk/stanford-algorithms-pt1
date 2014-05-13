c896ffda98a52f22942f0c2419384bf34ade568d
davidalk <david.alkanani@gmail.com>
2014-05-13 14:10:43 +0100

    build merge function

--- src/main/scala/week1/CountInversions.scala
+++ src/main/scala/week1/CountInversions.scala
@@ -10,6 +10,24 @@ object CountInversions {
 
   def loadFile(filename: String): Array[Int] = {
     val stringArray = Source.fromInputStream(getClass.getResourceAsStream("/" + filename)).getLines.toArray
-    stringArray.map(x => x.toInt)
+    stringArray.map(_.toInt)
+  }
+
+  def merge(a: Array[Int], b: Array[Int]): Array[Int] = {
+    val outputSize = a.length + b.length
+    val returnArray = Array.fill(outputSize)(0)
+    var aIndex, bIndex = 0
+    val aWithMarker = a :+ Int.MaxValue
+    val bWithMarker = b :+ Int.MaxValue
+    for (i <- 0 until outputSize) {
+      if (aWithMarker(aIndex) <= bWithMarker(bIndex)) {
+        returnArray(i) = aWithMarker(aIndex)
+        aIndex += 1
+      } else {
+        returnArray(i) = bWithMarker(bIndex)
+        bIndex += 1
+      }
+    }
+    returnArray
   }
 }
--- src/test/scala/week1/CountInversionsTest.scala
+++ src/test/scala/week1/CountInversionsTest.scala
@@ -4,9 +4,17 @@ import org.scalatest._
 
 class CountInversionsTest extends FlatSpec {
 
-  "A source file" should "be loaded and returned as an array" in {
+  "The source file" should "be loaded and returned as an array" in {
     val expected = Array(12, 18, 55, 220, 1250)
     val result = CountInversions.loadFile("test_file.txt")
+    assert(expected.deep == result.deep)
+  }
+  
+  "The merge function" should "combine two sorted arrays and return an ordered array" in {
+    val a = Array(3, 8, 10, 50, 100)
+    val b = Array(2, 15, 50, 88, 92, 200, 350)
+    val expected = Array(2, 3, 8, 10, 15, 50, 50, 88, 92, 100, 200, 350)
+    val result = CountInversions.merge(a, b)
     assert(expected.deep == result.deep)
   }
 
