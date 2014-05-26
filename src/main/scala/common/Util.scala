eb427ff13c0059bbd6145ffb05383c261cd588f2
davidalk <david.alkanani@gmail.com>
2014-05-26 22:27:12 +0100

    build loadGraph function

--- src/main/scala/common/Util.scala
+++ src/main/scala/common/Util.scala
@@ -1,12 +1,23 @@
 package common
 
 import scala.io.Source
+import scala.collection.mutable.HashMap
 
 object Util {
-  
-    def loadFile(filename: String): Array[Int] = {
+
+  def loadFile(filename: String): Array[Int] = {
     val stringArray = Source.fromInputStream(getClass.getResourceAsStream("/" + filename)).getLines.toArray
     stringArray.map(_.toInt)
   }
 
+  def loadGraph(filename: String): Map[Int, List[Int]] = {
+    val rows = Source.fromInputStream(getClass.getResourceAsStream("/" + filename)).getLines
+    val returnMap = HashMap[Int, List[Int]]()
+    rows.foreach(s => {
+      val rowList = s.split("\t").toList
+      returnMap.put(rowList.head.toInt, rowList.tail.map(_.toInt))
+    })
+
+    returnMap.toMap
+  }
 }
--- src/test/scala/common/UtilTest.scala
+++ src/test/scala/common/UtilTest.scala
@@ -1,13 +1,29 @@
 package common
 
+import scala.collection.immutable.HashMap
 import org.scalatest._
 
 class UtilTest extends FlatSpec {
 
-    "The source file" should "be loaded and returned as an array" in {
+  "The source file" should "be loaded and returned as an array" in {
     val expected = Array(12, 18, 55, 220, 1250)
     val result = Util.loadFile("test_file.txt")
     assert(expected.deep == result.deep)
   }
-    
+
+  "The graph file" should "be loaded as a map of lists" in {
+    val expected = HashMap[Int, List[Int]](
+      1 -> List(2, 3, 4),
+      2 -> List(1, 4),
+      3 -> List(1, 4),
+      4 -> List(1, 2, 3))
+
+    val result = Util.loadGraph("test_graph_data.txt")
+
+    expected.foreach {
+      case (k, v) =>
+        assert(v == result(k))
+    }
+  }
+
 }
