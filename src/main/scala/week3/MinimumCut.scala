e23419ce08ca68e92fe6570dde9f8ff29384c999
davidalk <david.alkanani@gmail.com>
2014-05-27 20:19:17 +0100

    fix contract vertices

--- src/main/scala/week3/MinimumCut.scala
+++ src/main/scala/week3/MinimumCut.scala
@@ -1,14 +1,31 @@
 package week3
 
 import scala.collection.mutable.HashMap
+import scala.util.Random
 
 object MinimumCut {
 
   def contractVertices(v1: Int, v2: Int, graph: Map[Int, List[Int]]): Map[Int, List[Int]] = {
-    var newList = graph(v1)
-    newList = newList ::: graph(v2).filter(_ != v1)
-    val returnMap = graph.filter{ case (k, v) => k!=v2  }
-    returnMap.updated(v1, newList)
+    var map = graph
+    for(key <-  graph(v2)) {
+      val listToUpdate = map(key)
+      map = map.updated(key, listToUpdate.map(i => if(i==v2) v1 else i))
+    }
+    val newV1List = (map(v1) ::: graph(v2)).filter(_ != v1)
+    map = map.updated(v1, newV1List)
+    map - v2
   }
 
+  def contractGraph(graph: Map[Int, List[Int]]): Map[Int, List[Int]] = {
+    if (graph.size == 2) {
+      graph
+    }
+    else {
+      val keys = graph.keySet.toList
+      val v1 = keys(Random.nextInt(keys.size))
+      val remainingKeys = keys.filter(_ != v1)
+      val v2 = remainingKeys(Random.nextInt(remainingKeys.size))
+      contractGraph(contractVertices(v1, v2, graph))
+    }
+  }
 }
--- src/test/scala/week3/MinimumCutTest.scala
+++ src/test/scala/week3/MinimumCutTest.scala
@@ -8,18 +8,27 @@ class MinimumCutTest extends FlatSpec {
 
   "The contract function" should "merge vertices correctly without self loops" in {
     val expected = HashMap[Int, List[Int]](
-      1 -> List(2, 3, 4, 2, 3),
-      2 -> List(1, 4),
-      3 -> List(1, 4))
+      1 -> List(2, 3, 2, 3),
+      2 -> List(1, 1),
+      3 -> List(1, 1))
 
     val graph = Util.loadGraph("test_graph_data.txt")
 
     val result = MinimumCut.contractVertices(1, 4, graph)
-
+    
     expected.foreach {
       case (k, v) =>
         assert(v == result(k))
     }
   }
 
+  "The contractGraph function" should "contract all edges leaving only one" in {
+    val graph = Util.loadGraph("test_graph_data.txt")
+    
+    val result = MinimumCut.contractGraph(graph)
+    
+    result.foreach{case (k, v) => println("Key: " + k + " List: " + v)}
+    
+    assert(result.size == 2)
+  }
 }
