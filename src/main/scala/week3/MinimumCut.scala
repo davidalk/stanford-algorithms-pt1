bb94aa57fc89cdb5c9b219c3c96b676b4a36598f
davidalk <david.alkanani@gmail.com>
2014-05-27 21:03:57 +0100

    complete main algorithm

--- src/main/scala/week3/MinimumCut.scala
+++ src/main/scala/week3/MinimumCut.scala
@@ -2,8 +2,29 @@ package week3
 
 import scala.collection.mutable.HashMap
 import scala.util.Random
+import scala.math._
+import common.Util
 
 object MinimumCut {
+  
+  def main(args: Array[String]): Unit = {
+    var minCut = Long.MaxValue
+    val graph = Util.loadGraph("kargerMinCut.txt")
+    val trials = round(ceil(pow(graph.size, 2) * log(graph.size)))
+    
+    println("Number of trials to perform: " + trials + "\n")
+    
+    for(i <- 0L to trials) {
+      val trialCut = contractGraph(graph)
+      val (_, trialCutList) = trialCut.head
+      
+      if(trialCutList.size < minCut) minCut = trialCutList.size
+      
+      if(i % 100 == 0) print(". " + minCut + " ")
+    }
+    
+    println("Min cut is " + minCut)
+  }
 
   def contractVertices(v1: Int, v2: Int, graph: Map[Int, List[Int]]): Map[Int, List[Int]] = {
     var map = graph
@@ -23,8 +44,8 @@ object MinimumCut {
     else {
       val keys = graph.keySet.toList
       val v1 = keys(Random.nextInt(keys.size))
-      val remainingKeys = keys.filter(_ != v1)
-      val v2 = remainingKeys(Random.nextInt(remainingKeys.size))
+      val edges = graph(v1)
+      val v2 = edges(Random.nextInt(edges.size))
       contractGraph(contractVertices(v1, v2, graph))
     }
   }
