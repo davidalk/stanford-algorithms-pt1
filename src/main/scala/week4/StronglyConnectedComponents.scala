150ed577e70cc1b72c3c22ee8dec379738cbebb3
davidalk <david.alkanani@gmail.com>
2014-06-08 15:53:12 +0100

    commit

--- src/main/scala/week4/StronglyConnectedComponents.scala
+++ src/main/scala/week4/StronglyConnectedComponents.scala
@@ -8,14 +8,54 @@ object StronglyConnectedComponents {
 
   def main(args: Array[String]): Unit = {
     val graph = Util.loadDirectedGraph("SCC.txt")
-    val result = dfs(graph)
+
+    val order = dfs(graph)
+    println(order)
+    val invertedGraph = Util.loadInvertedDirectedGraph("SCC.txt")
+    val scc = dfsSccCount(invertedGraph, order)
+    println(scc.sorted.reverse)
+
   }
 
-  def dfs(graph: Map[Int, List[Int]]): List[Int] = {
+  def dfsSccCount(graph: Map[Int, List[Int]], order: List[Int]): List[Int] = {
+    println("starting scc count")
+
     val defaultGraph = graph.withDefault(_ => List())
 
     val visited = scala.collection.mutable.HashMap[Int, Boolean]().withDefault(_ => false)
+
+    var sccList = List[Int]()
+
+    def dfsVisit(u: Int): Int = {
+      val stack = Stack[Int]()
+      stack.push(u)
+      var count = 0
+
+      while (!stack.isEmpty) {
+        val v = stack.pop
+        if (!visited(v)) {
+          count = count + 1
+          visited(v) = true
+          defaultGraph(v) map { stack.push(_) }
+        }
+      }
+      count
+    }
+
+    for (vertex <- order) {
+      if (!visited(vertex)) {
+        println("Start visit at " + vertex)
+        sccList = sccList :+ dfsVisit(vertex)
+      }
+    }
+
+    sccList
+  }
+
+  def dfs(graph: Map[Int, List[Int]]): List[Int] = {
     var order = List[Int]()
+    val defaultGraph = graph.withDefault(_ => List())
+    val visited = scala.collection.mutable.HashMap[Int, Boolean]().withDefault(_ => false)
 
     def dfsVisit(u: Int): Unit = {
       val stack = Stack[Int]()
@@ -39,7 +79,7 @@ object StronglyConnectedComponents {
     }
 
     order
+
   }
-  
 
 }
