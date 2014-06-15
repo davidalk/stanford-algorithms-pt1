e9c0030ecd8c78fde4387a1d54e89b7d04c627c1
davidalk <david.alkanani@gmail.com>
2014-06-15 02:40:52 +0100

    init

---
+++ dijkstra/src/main/java/stanford1/week5/dijkstra/App.java
@@ -0,0 +1,13 @@
+package stanford1.week5.dijkstra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
